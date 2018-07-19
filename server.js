c1f6cf2459c7a3c0c9327696b625c2688c8b1edf
guruprasath-s <34478894+guruprasath-s@users.noreply.github.com>
2018-07-19 20:23:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var connection = mysql.createConnection({
     host: 'sql12.freesqldatabase.com',
     user: 'sql12248317',
